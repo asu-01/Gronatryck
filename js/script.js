f64871d957ee10008afde00a919a5f50619b1f15
asu-01 <alva.sundberg@gmail.com>
2024-10-18 14:38:38 +0200

    Mer fix på startsidan
    Co-authored-by: Olivia Hansson  <ooliviahh@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -120,6 +120,18 @@ for (const product of productArray) {
   }
 }
 
+/* Dropdown eventlyssnare och funktion */
+const dropdown = document.getElementById("dropdownContainer");
+const dropdownProducts = document.getElementById("dropdownProducts");
+
+dropdownProducts.addEventListener("mouseenter", () => {
+  dropdown.style.display = "block";
+});
+
+dropdownProducts.addEventListener("mouseout", () => {
+  dropdown.style.display = "none";
+});
+
 // for (let i = 0; i < productArray.length; i++) {}
 
 // Ta bort data
@@ -127,3 +139,26 @@ for (const product of productArray) {
 
 // Uppdatera data
 // localStorage.setItem()
+
+// ----------------    animation grid css 'vilka är vi' --------------------//
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Get all thumbnail images
+  const thumbnails = document.querySelectorAll(".thumbnail-image");
+  const container = document.querySelector(".thumbnail-container");
+
+  thumbnails.forEach((thumbnail) => {
+    thumbnail.addEventListener("click", function () {
+      // Remove the "large" class from all images
+      thumbnails.forEach((img) =>
+        img.classList.remove("thumbnail-image--large")
+      );
+
+      // Add the "large" class to the clicked image
+      this.classList.add("thumbnail-image--large");
+
+      // Reorder images: move clicked image to the first position
+      container.prepend(this);
+    });
+  });
+});
